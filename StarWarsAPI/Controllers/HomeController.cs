eb72e0639f953d3371c1630561c7f6c62b0060b4
Christina Lambert <clambert0615@gmail.com>
2020-06-03 12:28:36 -0400

    Initial submit for working with APIs

---
+++ StarWarsAPI/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StarWarsAPI.Models;
+
+namespace StarWarsAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly StarWarsDAL SD = new StarWarsDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            People p = SD.GetPeople();
+            
+            return View(p);
+            
+        }
+        public IActionResult PersonSearch(int Id)
+        {
+            Person p = SD.GetPerson("people", Id);
+            return View(p);
+        }
+        public IActionResult PlanetSearch(int Id)
+        {
+            Planet pl = SD.GetPlanet("planets", Id);
+                return View(pl);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StarWarsAPI/Models/StarWarsDAL.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace StarWarsAPI.Models
+{
+    public class StarWarsDAL
+    {
+
+        public string GetAPIJson(string endpoint, int Id)
+        {
+            string url = $"https://swapi.dev/api/{endpoint}/{Id}/";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Convert the response into something useable 
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            string output = sr.ReadToEnd();
+            return output;
+        }
+
+        public Person GetPerson(string endpoint, int Id)
+        {
+            string personData = GetAPIJson(endpoint, Id);
+            Person p = JsonConvert.DeserializeObject<Person>(personData);
+            return p;
+
+        }
+        public Planet GetPlanet(string endpoint, int Id)
+        {
+            string planetData = GetAPIJson(endpoint, Id);
+            Planet pl = JsonConvert.DeserializeObject<Planet>(planetData);
+            return pl;
+        }
+
+        public string GetAPIJson(string endpoint)
+        {
+            string url = $"https://swapi.dev/api/{endpoint}/";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Convert the response into something useable 
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            string output = sr.ReadToEnd();
+            return output;
+        }
+        public People GetPeople()
+        {
+            string endpointData = GetAPIJson("people");
+
+            return JsonConvert.DeserializeObject<People>(endpointData);
+        }
+
+      
+    }
+}
